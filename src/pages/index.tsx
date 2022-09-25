37344bd92467b4a1e4806387d51fd6b05c52397c
Maick Souza <maick_a_s@msn.com>
2022-09-25 10:05:27 -0300

    chore: setting stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,26 @@
-import type { NextPage } from "next";
+import { styled } from "../styles";
 
-const Home: NextPage = () => {
+const Button = styled("button", {
+  backgroundColor: "$rocketseat",
+  borderRadius: 4,
+  border: 0,
+  padding: "4px 8px",
+  cursor: "pointer",
+
+  span: {
+    fontWeight: "bold",
+  },
+
+  "&:hover": {
+    filter: "brightness(0.8)",
+  },
+});
+
+export default function Home() {
   return (
-    <div>
-      <h1>Maick Souza</h1>
-    </div>
+    <Button>
+      <span>Teste</span>
+    </Button>
   );
-};
-
-export default Home;
+}
 
