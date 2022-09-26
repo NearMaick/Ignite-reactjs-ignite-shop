eb4f619a99007f97050869c1ed1610cadc8b69c4
Maick Souza <maick_a_s@msn.com>
2022-09-26 09:04:13 -0300

    feat(web): setting caroussel slide

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,24 @@
+import { useKeenSlider } from "keen-slider/react";
 import Image from "next/image";
-import shirt01 from "../assets/shirts/1.png";
-import shirt02 from "../assets/shirts/2.png";
+
 import { HomeContainer, Product } from "../styles/pages/home";
 
+import shirt01 from "../assets/shirts/1.png";
+import shirt02 from "../assets/shirts/2.png";
+
+import "keen-slider/keen-slider.min.css";
+
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className='keen-slider'>
+      <Product className='keen-slider__slide'>
         <Image src={shirt01} alt='' width={520} height={480} />
 
         <footer>
@@ -15,7 +27,7 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className='keen-slider__slide'>
         <Image src={shirt02} alt='' width={520} height={480} />
 
         <footer>
